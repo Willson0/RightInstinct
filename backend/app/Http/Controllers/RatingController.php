2ae41e976a7acef8edf6e59897e5e6c4f0295069
Willson0 <logvinovnikita3@gmail.com>
2025-05-10 11:25:41 +0500

    06 05 2025

---
+++ backend/app/Http/Controllers/RatingController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Favourite\FavouriteStoreRequest;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RatingController extends Controller
+{
+    public function rate (FavouriteStoreRequest $request) {
+        $user = User::where("telegram_id", $request["initData"]["user"]["id"])->firstOrFail();
+
+        $tables = [
+            "post" => "posts",
+            "service" => "services",
+            "event" => "events",
+            "user" => "users",
+        ];
+        $table = $tables[$request["type"]];
+
+        $review = Review::where("user_id", $user->id)->where("type", $request["type"])
+            ->where("object_id", $request["object_id"])->first();
+        if ($review) {
+            $review->rating = $request["rating"];
+            $review->save();
+        } else {
+            $review = Review::create([
+                "user_id" => $user->id,
+                "object_id" => $request["object_id"],
+                "type" => $request["type"],
+                "rating" => $request["rating"],
+            ]);
+        }
+        DB::table($table)->where("object_id", $request["object_id"])->update([
+            "rating" => Review::where("type", $request["type"])
+                ->where("object_id", $request["object_id"])->avg("rating"),
+        ]);
+        return response()->json($review);
+    }
+}
---
+++ backend/app/Http/Requests/Rating/RatingRateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Rating;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RatingRateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "type" => "required|string|in:post,service,event,user",
+            "object_id" => "required|integer",
+            "rating" => "required|float",
+        ];
+    }
+}
